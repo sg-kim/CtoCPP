b3cdf40f9c17d5ffe74af9798fdf00d4cd82e48e
sg-kim <sgkim08@gmail.com>
2018-09-21 06:49:46 +0900

    the first understanding of using template sytax

---
+++ CtoCpp_hex_game_hw_lec6/CtoCpp_hex_game_hw_lec6/hexBoard.cpp
@@ -0,0 +1,14 @@
+#include "hexBoard.h"
+
+int hexBoard::isWinner()
+{
+}
+
+int hexBoard::setPanel(unsigned short xpos, unsigned short ypos, unsigned short color)
+{
+	//	set position(x, y) and color of a hex panel in the board
+
+	//	examine whether or not there are neighbor friendly hex panel by examining a player(= color)'s paths graphs
+
+	//	if there is a friendly hexPanel, expand the player's path graph or create new graph
+}
---
+++ CtoCpp_hex_game_hw_lec6/CtoCpp_hex_game_hw_lec6/hexBoard.h
@@ -0,0 +1,32 @@
+#ifndef _HEXBOARD_
+
+#define _HEXBOARD_
+
+#include "hexPanel.h"
+
+class hexBoard
+{
+private:
+	unsigned short boardSize;
+	hexPanel** gameBoard;
+	hexPanel* player;
+
+public:
+	hexBoard(unsigned short size):boardSize(size)
+	{
+		gameBoard = new hexPanel*[boardSize];
+		for(int i = 0; i < boardSize; i++){
+			gameBoard[i] = new hexPanel[boardSize];
+		}
+
+		player = new hexPanel[2];
+	}
+	~hexBoard(){}
+	int isWinner();	//	is there a winner?
+	int setPanel(unsigned short xpos, unsigned short ypos, unsigned short color);	//	set a panel in the game board
+
+
+};
+
+
+#endif // !_HEXBOARD_
