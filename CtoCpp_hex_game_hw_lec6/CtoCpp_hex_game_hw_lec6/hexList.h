8d169e4a8e00cb4fcc831aab3eb383058827652b
sg-kim <sgkim08@gmail.com>
2018-09-21 06:49:46 +0900

    the first understanding of using template sytax

--- CtoCpp_hex_game_hw_lec6/CtoCpp_hex_game_hw_lec6/hexList.h
+++ CtoCpp_hex_game_hw_lec6/CtoCpp_hex_game_hw_lec6/hexList.h
@@ -8,25 +8,58 @@ class hexList
 {
 private:
 	hexPanel panel;
-	hexList* next;
+	hexList* sibling;
 	hexList* child;
 	hexList* head;
 	hexList* cursor;
 
 public:
-	hexList():next(nullptr), child(nullptr), head(nullptr), cursor(nullptr){}
+	hexList():sibling(nullptr), child(nullptr), head(nullptr), cursor(nullptr){}
 	~hexList()
 	{
+		hexList* preHead;
+		hexList* preCursor;
+
 		cursor = head;
 
-		while(cursor->next != nullptr){
-			cursor = cursor->next;
+		while(cursor->sibling != nullptr){	//	delete siblings
+
+			while(cursor->sibling != nullptr){
+				preHead = cursor;	//	just before the last sibling
+				cursor = cursor->sibling;	//	find the last sibling
+			}
+
+			while(cursor->child != nullptr){
+
+				while(cursor->child != nullptr){
+					preCursor = cursor;	//	just before the last child
+					cursor = cursor->child;	//	find the last child
+				}
+				delete cursor;
+				preCursor->child = nullptr;
+
+				cursor = preHead->sibling;	//	head of children = a sibling
+			}
+
+			delete cursor;	//	delete the head of children = delete a sibling
+			preHead->sibling = nullptr;
+
+			cursor = head;	//	head of siblings
 		}
 
-		while(cursor->child != nullptr){
-			cursor = cursor->child;
+		while(cursor->child != nullptr){	//	delete the children of the origin
+
+			while(cursor->child != nullptr){
+				preCursor = cursor;	//	just before the last child
+				cursor = cursor->child;	//	find the last child
+			}
+			delete cursor;
+			preCursor->child = nullptr;
+
+			cursor = preHead->sibling;	//	head of children = a sibling
 		}
 
+		delete cursor;	//	delete the last sibling
 	}
 	void append(hexPanel* panel);
 	void getLength();
