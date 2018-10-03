b7913c0760a52170d339d44b0796be0bb14e7729
sg-kim <sgkim08@gmail.com>
2018-10-03 16:35:50 +0900

    exercise on rvalue reference - lecture 7

--- CtoCpp_lvalue_rvalue_lec7/CtoCpp_lvalue_rvalue_lec7/CtoCpp_lvalue_rvalue_lec7.cpp
+++ CtoCpp_lvalue_rvalue_lec7/CtoCpp_lvalue_rvalue_lec7/CtoCpp_lvalue_rvalue_lec7.cpp
@@ -4,30 +4,60 @@
 #include "stdafx.h"
 
 #include <iostream>
+#include <vector>
 
-void efficientSwap(int& a, int&& b)
+using namespace std;
+
+template<typename T>
+void efficientMove(vector<T>& a, vector<T>&& b)
 {
-
+	a = b;
+	b = nullptr;
 }
 
+int foo()
+{
+	return 10;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int a = 0;
+	
+	int a = foo();
 	int& b = a;
-	int&& c = 0;
+	int&& c = foo();
 
-	//b = 1;
-	a = a + 1;
-	b = b + 1;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
 
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << c << std::endl;
+	vector<int> va(5);
+	vector<int> vb(5);
+
+	for(int i = 0; i < 5; i++){
+		va[i] = i;
+		vb[i] = i + 5;
+	}
+
+	for(int i = 0; i < 5; i++){
+		cout << va[i] << endl;
+	}
+
+	for(int i = 0; i < 5; i++){
+		cout << vb[i] << endl;
+	}
+
+	efficientMove(va, vb);
 	
-	c = b;
-	
-	std::cout << c << std::endl;
+	for(int i = 0; i < 5; i++){
+		cout << va[i] << endl;
+	}
+
+	for(int i = 0; i < 5; i++){
+		cout << vb[i] << endl;
+	}
+
 
 	return 0;
 }
+
