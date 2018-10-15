8349b20cec1c5162d13269e09f15372e92d49038
sg-kim <sgkim08@gmail.com>
2018-10-15 21:03:42 +0900

    implement hex game using MC as a move algorithm

--- CtoCpp_hex_game_hw_lec6/CtoCpp_hex_game_hw_lec6/hexList.h
+++ CtoCpp_hex_game_hw_lec6/CtoCpp_hex_game_hw_lec6/hexList.h
@@ -2,7 +2,12 @@
 
 #define _HEXLIST_
 
+#include <iostream>
+
 #include "hexPanel.h"
+
+using std::cout;
+using std::endl;
 
 class hexList
 {
@@ -79,5 +84,89 @@ public:
 	void resetCursor();
 	void display();
 };
+
+void hexList::append(hexPanel* panel, unsigned short boardSize)
+{
+	if(head == nullptr){
+		head = new hexPanel(panel->getXpos(), panel->getYpos(), panel->getColor(), nullptr, boardSize);
+		cursor = head;
+	}
+	else{
+		resetCursor();
+
+		while(cursor->getNext() != nullptr){
+			cursor = cursor->getNext();
+		}
+
+		cursor->setNext(new hexPanel(panel->getXpos(), panel->getYpos(), panel->getColor(), nullptr, boardSize));
+	}
+}
+
+int hexList::getLength()
+{
+	int count = 0;
+
+	resetCursor();
+
+	while(cursor != nullptr){
+		
+		cursor = cursor->getNext();
+		count++;
+	}
+
+	return count;
+}
+
+hexPanel* hexList::getHexPanel()
+{
+	return cursor;
+}
+
+hexList* hexList::getNext()
+{
+	return next;
+}
+
+void hexList::setNext(hexList* nextPtr)
+{
+	next = nextPtr;
+}
+
+hexPanel* hexList::search(unsigned short xPos, unsigned short yPos, short color)
+{
+	hexPanel* retVal = nullptr;
+	
+	resetCursor();
+
+	while(cursor != nullptr){
+
+		if((cursor->getXpos() == xPos) && (cursor->getYpos() == yPos) && (cursor->getColor() == color)){
+			retVal = cursor;
+			break;
+		}
+
+		cursor = cursor->getNext();
+	}
+
+	return retVal;
+}
+
+void hexList::resetCursor()
+{
+	cursor = head;
+}
+
+void hexList::display()
+{
+	resetCursor();
+
+	while(cursor != nullptr){
+
+		cout << "(" << cursor->getXpos() << ", " << cursor->getYpos() << ", " << cursor->getColor() << ")";
+		cout << "-->";
+
+		cursor = cursor->getNext();
+	}
+}
 
 #endif // !_HEXLIST_
--- CtoCpp_hex_game_hw_lec6/CtoCpp_hex_game_hw_lec6/hexListList.h
+++ CtoCpp_hex_game_hw_lec6/CtoCpp_hex_game_hw_lec6/hexListList.h
@@ -2,7 +2,12 @@
 
 #define _HEXLISTLIST_
 
+#include <iostream>
+
 #include "hexList.h"
+
+using std::cout;
+using std::endl;
 
 class hexListList
 {
@@ -30,7 +35,97 @@ public:
 	int getLength();
 	void resetCursor();
 	void display();
+	void deleteList(hexList* targetList);
 };
+
+void hexListList::append(hexPanel* panel, unsigned short boardSize)
+{
+	if(head == nullptr){
+		head = new hexList();
+		cursor = head;
+		cursor->append(panel, boardSize);
+	}
+	else{
+		resetCursor();
+
+		while(cursor->getNext() != nullptr){
+			cursor = cursor->getNext();
+		}
+
+		cursor->setNext(new hexList());
+		cursor = cursor->getNext();	//	newly created list
+		cursor->append(panel, boardSize);
+	}
+}
+
+hexList* hexListList::getList()
+{
+	return cursor;
+}
+
+int hexListList::getLength()
+{
+	int count = 0;
+
+	resetCursor();
+
+	while(cursor != nullptr){
+
+		count++;
+
+		cursor = cursor->getNext();
+	}
+
+	return count;
+}
+
+void hexListList::resetCursor()
+{
+	cursor = head;
+}
+
+void hexListList::display()
+{
+	resetCursor();
+
+	while(cursor != nullptr){
+		cout << "{ ";
+		cursor->display();
+		cout << " } ==> ";
+
+		cursor = cursor->getNext();
+	}
+	cout << endl;
+}
+
+void hexListList::deleteList(hexList* targetList)
+{
+	hexList* prevList = nullptr;
+
+	if(targetList == head){
+		head = targetList->getNext();
+		delete targetList;
+	}
+	else{
+		resetCursor();
+
+		while(cursor != nullptr){
+
+			if(cursor == targetList){
+				break;
+			}
+			else{
+				prevList = cursor;
+				cursor = cursor->getNext();
+			}
+		}
+
+		if(cursor != nullptr){
+			prevList->setNext(cursor->getNext());
+			delete cursor;
+		}
+	}
+}
 
 #endif // !_HEXLISTLIST_
 
