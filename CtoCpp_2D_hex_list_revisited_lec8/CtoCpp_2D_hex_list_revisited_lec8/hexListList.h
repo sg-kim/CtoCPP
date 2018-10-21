92c3d0cc306be975dea727e4ad5af2c3d011cbe3
sg-kim <sgkim08@gmail.com>
2018-10-22 07:16:18 +0900

    exercise on 2D linked list structure

---
+++ CtoCpp_2D_hex_list_revisited_lec8/CtoCpp_2D_hex_list_revisited_lec8/hexListList.h
@@ -0,0 +1,113 @@
+#ifndef _HEX_LIST_LIST_
+
+#define _HEX_LIST_LIST_
+
+#include <iostream>
+
+#include "hexList.h"
+
+using std::cout;
+using std::endl;
+
+class hexListList
+{
+private:
+	hexList* head;
+	hexList* cursor;
+	hexListList* next;
+
+public:
+	hexListList(): head(nullptr), cursor(nullptr), next(nullptr) {}
+	hexListList(hexListList& ll)	//	shallow copy
+	{
+		head = ll.getHead();
+		cursor = ll.getCursor();
+		next = ll.getNext();
+	}
+	~hexListList() {}
+	void copyhexListList(hexListList* ll);	//	deep copy
+	void resetCursor();
+	void appendList(hexList& l);
+	void append(hexListList& ll);
+	hexList* getHead();
+	hexList* getCursor();
+	hexListList* getNext();
+	void print();
+};
+
+void hexListList::copyhexListList(hexListList* ll)	//	deep copy
+{
+	if(ll != nullptr){
+
+		hexList* l;
+
+		ll->resetCursor();
+
+		l = ll->getCursor();		//	target list
+
+		head = new hexList();
+		head->copyHexList(l);
+
+		resetCursor();
+
+		l = l->getNext();
+
+		while(l != nullptr){
+
+			cursor->append(*(new hexList()));
+			cursor = cursor->getNext();
+			cursor->copyHexList(l);
+
+			l = l->getNext();
+		}
+	}
+}
+
+void hexListList::resetCursor()
+{
+	cursor = head;
+}
+
+void hexListList::appendList(hexList& l)
+{
+	resetCursor();
+
+	if(cursor == nullptr){
+		head = &l;
+		cursor = head;
+	}
+	else{
+
+		while(cursor->getNext() != nullptr){
+
+			cursor = cursor->getNext();
+		}
+
+		cursor->append(l);
+	}
+}
+
+void hexListList::append(hexListList& ll)
+{
+	next = &ll;
+}
+
+hexList* hexListList::getHead()	{	return head;	}
+
+hexList* hexListList::getCursor()	{	return cursor;	}
+
+hexListList* hexListList::getNext()	{	return next;	}
+
+void hexListList::print()
+{
+	resetCursor();
+
+	while(cursor != nullptr){
+		cursor->print();
+		cout << " ==> ";
+		cursor = cursor->getNext();
+	}
+	cout << endl;
+}
+
+#endif // !_HEX_LIST_LIST_
